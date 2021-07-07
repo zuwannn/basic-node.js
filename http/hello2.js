18e01fec36d5f77d076e171861de426e198fad02
zuwannn <suwanneethongtha@gmail.com>
2021-07-07 13:28:20 +0700

    Update hello2.js

--- http/hello2.js
+++ http/hello2.js
@@ -15,3 +15,12 @@ var app = http.createServer(function(req, res){
 app.listen(3000, ipaddress,function(){
     console.log('Node server started on '+ ipaddress +':'+app.address().port);
 });
+
+// การทำงาน
+// เมื่อ server พร้อมที่จะรอ request แล้ว จะแสดงข้อความทาง cmd 
+// และจะรอ request อยู่ที่ http://localhost:3000 
+// และเมื่อได้รับ request แล้ว จะส่ง header ให้ client 
+// โดยมี status code = 200, Content-type เป็น text/html 
+// และแสดงข้อความว่า Hello World! 
+
+// ไม่ว่าจะเปลี่ยน path เป็นอะไร หรือใช้ query แต่การแสดงผลก็ไม่เปลี่ยนแปลง 
